9a30f769176f1c22674de22513c23445f95172fb
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-20 09:51:53 +0300

    remove feature added to shoppinglist.js

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,52 +2,59 @@ import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-const [cartItems, setCartItems] = useState([]);
-const [selectedCategory, setSelectedCategory] = useState("All"); // Initialize selectedCategory to "All"
+  const [cartItems, setCartItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("All"); // Initialize selectedCategory to "All"
 
-const addToCart = (name, category) => {
-const newItem = { name, category };
-setCartItems([...cartItems, newItem]);
-};
+  const addToCart = (name, category) => {
+    const newItem = { name, category };
+    setCartItems([...cartItems, newItem]);
+  };
 
-const handleSelectChange = (event) => {
-setSelectedCategory(event.target.value);
-};
+  const removeFromCart = (index) => {
+    const updatedCartItems = [...cartItems];
+    updatedCartItems.splice(index, 1);
+    setCartItems(updatedCartItems);
+  };
 
-return (
-<div className="ShoppingList">
-<div className="Filter">
-<select name="filter" onChange={handleSelectChange} value={selectedCategory}>
-<option value="All">Filter by category</option>
-<option value="Produce">Produce</option>
-<option value="Dairy">Dairy</option>
-<option value="Dessert">Dessert</option>
-</select>
-</div>
-<ul className="Items">
-{items
-.filter((item) => selectedCategory === "All" || item.category === selectedCategory) // Filter items based on selectedCategory
-.map((item) => (
-<Item
-           key={item.id}
-           name={item.name}
-           category={item.category}
-           addToCart={addToCart}
-         />
-))}
-</ul>
-<div className="Cart">
-<h2>Cart</h2>
-<ul>
-{cartItems.map((item, index) => (
-<li key={index}>
-{item.name} ({item.category})
-</li>
-))}
-</ul>
-</div>
-</div>
-);
+  const handleSelectChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  return (
+    <div className="ShoppingList">
+      <div className="Filter">
+        <select name="filter" onChange={handleSelectChange} value={selectedCategory}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </div>
+      <ul className="Items">
+        {items
+          .filter((item) => selectedCategory === "All" || item.category === selectedCategory) // Filter items based on selectedCategory
+          .map((item) => (
+            <Item
+              key={item.id}
+              name={item.name}
+              category={item.category}
+              addToCart={addToCart}
+            />
+          ))}
+      </ul>
+      <div className="Cart">
+        <h2>Cart</h2>
+        <ul>
+          {cartItems.map((item, index) => (
+            <li key={index}>
+              {item.name} ({item.category}){" "}
+              <button onClick={() => removeFromCart(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default ShoppingList;
